46326f7115a17a87e3719b28c975634efd6f5878
Jacob Phillips <jifalops@gmail.com>
2018-09-23 12:55:53 -0400

    support null values

--- lib/shared_theme_flutter.dart
+++ lib/shared_theme_flutter.dart
@@ -10,41 +10,50 @@ export 'package:shared_theme/shared_theme.dart';
 export 'package:shared_theme_flutter/src/util.dart';
 
 /// Sets the default theme.
-void setTheme(sh.Theme theme) => currentTheme = theme;
-sh.Theme currentTheme;
+void setTheme(sh.Theme theme) => _currentTheme = theme;
+sh.Theme get currentTheme => _currentTheme;
+sh.Theme _currentTheme;
 
-ThemeData themeData(sh.Theme t,
-    [TargetPlatform platform = TargetPlatform.android]) {
+ThemeData themeData(sh.Theme t, [TargetPlatform platform]) {
   final base = t.brightness == sh.Brightness.light
       ? ThemeData.light()
       : ThemeData.dark();
   return base.copyWith(
-    platform: platform,
-    primaryColor: getColor(t.colors.primary),
-    primaryColorLight: getColor(t.colors.primaryLight),
-    primaryColorDark: getColor(t.colors.primaryDark),
-    accentColor: getColor(t.colors.secondary),
-    indicatorColor: getColor(t.colors.secondary),
-    backgroundColor: getColor(t.colors.background),
-    scaffoldBackgroundColor: getColor(t.colors.background),
-    cardColor: getColor(t.colors.surface),
-    dialogBackgroundColor: getColor(t.colors.surface),
-    dividerColor: getColor(t.colors.divider),
-    errorColor: getColor(t.colors.error),
-    textSelectionColor: getColor(t.colors.primaryLight),
-    textSelectionHandleColor: getColor(t.colors.primaryDark),
+    platform: platform ?? base.platform,
+    primaryColor: getColor(t.colors.primary, base.primaryColor),
+    primaryColorLight: getColor(t.colors.primaryLight, base.primaryColorLight),
+    primaryColorDark: getColor(t.colors.primaryDark, base.primaryColorDark),
+    accentColor: getColor(t.colors.secondary, base.accentColor),
+    indicatorColor: getColor(t.colors.indicator, base.indicatorColor),
+    backgroundColor: getColor(t.colors.background, base.backgroundColor),
+    scaffoldBackgroundColor:
+        getColor(t.colors.background, base.scaffoldBackgroundColor),
+    cardColor: getColor(t.colors.surface, base.cardColor),
+    dialogBackgroundColor:
+        getColor(t.colors.surface, base.dialogBackgroundColor),
+    dividerColor: getColor(t.colors.divider, base.dividerColor),
+    errorColor: getColor(t.colors.error, base.errorColor),
+    textSelectionColor:
+        getColor(t.colors.primaryLight, base.textSelectionColor),
+    textSelectionHandleColor:
+        getColor(t.colors.primaryDark, base.textSelectionHandleColor),
 
     /// Default color for [RaisedButton]s
-    buttonColor: color(t.elements.primaryButton.color),
-    buttonTheme: buttonTheme(t, t.elements.primaryButton),
-    inputDecorationTheme: inputDecorationTheme(t.elements.inputBase),
+    buttonColor: color(t.elements.primaryButton.color, base.buttonColor),
+    buttonTheme: buttonTheme(t, t.elements.primaryButton, base.buttonTheme),
+    inputDecorationTheme:
+        inputDecorationTheme(t.elements.inputBase, base.inputDecorationTheme),
     textTheme: textTheme(base.textTheme, t),
     primaryTextTheme: textTheme(base.primaryTextTheme, t),
     accentTextTheme: textTheme(base.accentTextTheme, t),
 
+    selectedRowColor: getColor(t.colors.selectedRow, base.selectedRowColor),
+    highlightColor: getColor(t.colors.highlight, base.highlightColor),
+    hintColor: getColor(t.colors.hint, base.hintColor),
+    splashColor: getColor(t.colors.splash, base.splashColor),
+
     // TODO
     // chipTheme: ,
-    // selectedRowColor: getColor(t.colors.divider),
   );
 }
 
@@ -66,6 +75,7 @@ Widget tertiaryButton(void Function() onPressed, {String text, Widget child}) {
       child: child, text: text);
 }
 
-/// Get a widget based on [currentTheme.elements.inputBase].
-Widget inputWidget(Widget input) =>
+/// Wrap a widget in padding if [currentTheme.elements.inputBase.margin] is
+/// non-zero.
+Widget wrapInput(Widget input) =>
     wrap(input, currentTheme.elements.inputBase.margin);
--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,41 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:shared_theme/shared_theme.dart' as sh;
 
-Color color(sh.Color c) => Color(c?.argb ?? sh.Colors.transparent.argb);
-Color getColor(sh.ContrastingColors colors) => Color(colors.color.argb);
-Color getContrast(sh.ContrastingColors colors) => Color(colors.contrast.argb);
+Color color(sh.Color c, [Color fallback]) =>
+    c == null ? (fallback ?? Colors.transparent) : Color(c.argb);
+Color getColor(sh.ContrastingColors colors, [Color fallback]) =>
+    color(colors.color, fallback);
+Color getContrast(sh.ContrastingColors colors, [Color fallback]) =>
+    color(colors.contrast, fallback);
 
-ButtonThemeData buttonTheme(sh.Theme theme, sh.Element button) =>
+ButtonThemeData buttonTheme(
+        sh.Theme theme, sh.Element button, ButtonThemeData fallback) =>
     ButtonThemeData(
-      height: button.size.minHeight,
-      minWidth: button.size.minWidth,
-      padding: edgeInsets(button.padding),
-      shape: shapeBorder(button),
+      height: button.size?.minHeight ?? fallback.height,
+      minWidth: button.size?.minWidth ?? fallback.minWidth,
+      padding: edgeInsets(button.padding, fallback.padding),
+      shape: shapeBorder(button, fallback.shape),
       textTheme: buttonTextTheme(theme, button),
     );
 
 ButtonTextTheme buttonTextTheme(sh.Theme theme, [sh.Element button]) {
   button ??= theme.elements.primaryButton;
-  return button.font.color == theme.colors.primary.color
+  return (button.font?.color) == theme.colors.primary.color
       ? ButtonTextTheme.primary
-      : button.font.color == theme.colors.secondary.color
+      : button.font?.color == theme.colors.secondary.color
           ? ButtonTextTheme.accent
           : ButtonTextTheme.normal;
 }
 
-EdgeInsets edgeInsets(sh.BoxSpacing spacing) => EdgeInsets.fromLTRB(
-    spacing.left, spacing.top, spacing.right, spacing.bottom);
+EdgeInsets edgeInsets(sh.BoxSpacing spacing, [EdgeInsets fallback]) =>
+    spacing == null
+        ? fallback
+        : EdgeInsets.fromLTRB(
+            spacing.left, spacing.top, spacing.right, spacing.bottom);
 
-ShapeBorder shapeBorder(sh.Element el) => el.border.hasRadius
-    ? RoundedRectangleBorder(
-        side: borderSide(el.border.bottom),
-        borderRadius: borderRadius(el.border))
-    : Border(
-        top: borderSide(el.border.top),
-        right: borderSide(el.border.right),
-        bottom: borderSide(el.border.bottom),
-        left: borderSide(el.border.left),
-      );
+ShapeBorder shapeBorder(sh.Element el, [ShapeBorder fallback]) =>
+    el.border == null
+        ? fallback
+        : el.border.hasRadius
+            ? RoundedRectangleBorder(
+                side: borderSide(el.border.bottom),
+                borderRadius: borderRadius(el.border))
+            : Border(
+                top: borderSide(el.border.top),
+                right: borderSide(el.border.right),
+                bottom: borderSide(el.border.bottom),
+                left: borderSide(el.border.left),
+              );
 
 BorderSide borderSide(sh.BorderSide side) => BorderSide(
     width: side.width,
@@ -45,29 +55,38 @@ BorderSide borderSide(sh.BorderSide side) => BorderSide(
 BorderStyle borderStyle(sh.BorderStyle style) =>
     style == sh.BorderStyle.solid ? BorderStyle.solid : BorderStyle.none;
 
-BorderRadius borderRadius(sh.Border border) => BorderRadius.only(
-    topLeft: radius(border.topLeft),
-    topRight: radius(border.topRight),
-    bottomRight: radius(border.bottomRight),
-    bottomLeft: radius(border.bottomLeft));
+BorderRadius borderRadius(sh.Border border, [BorderRadius fallback]) =>
+    border == null
+        ? fallback
+        : BorderRadius.only(
+            topLeft: radius(border.topLeft),
+            topRight: radius(border.topRight),
+            bottomRight: radius(border.bottomRight),
+            bottomLeft: radius(border.bottomLeft));
 
 Radius radius(sh.BorderRadius radius) => Radius.elliptical(radius.x, radius.y);
 
-InputDecorationTheme inputDecorationTheme(sh.Element input) =>
+InputDecorationTheme inputDecorationTheme(sh.Element input,
+        [InputDecorationTheme fallback]) =>
     InputDecorationTheme(
-      border: inputBorder(input.border),
-      // contentPadding: edgeInsets(input.padding),
-      fillColor: color(input.color),
-      filled: input.color != sh.Colors.transparent,
+      border: inputBorder(input.border, fallback.border),
+      contentPadding: edgeInsets(input.padding, fallback.contentPadding),
+      fillColor: color(input.color, fallback.fillColor),
+      filled: input.color == null
+          ? fallback.filled
+          : input.color != sh.Colors.transparent,
     );
 
-InputBorder inputBorder(sh.Border border) => border.hasUniformSides
-    ? OutlineInputBorder(
-        borderSide: borderSide(border.bottom),
-        borderRadius: borderRadius(border))
-    : UnderlineInputBorder(
-        borderSide: borderSide(border.bottom),
-        borderRadius: borderRadius(border));
+InputBorder inputBorder(sh.Border border, [InputBorder fallback]) =>
+    border == null
+        ? fallback
+        : border.hasUniformSides
+            ? OutlineInputBorder(
+                borderSide: borderSide(border.bottom),
+                borderRadius: borderRadius(border))
+            : UnderlineInputBorder(
+                borderSide: borderSide(border.bottom),
+                borderRadius: borderRadius(border));
 
 TextTheme textTheme(TextTheme base, sh.Theme t) => base.copyWith(
     display4: textStyle(t.fonts.display4, base.display4),
@@ -85,22 +104,26 @@ TextTheme textTheme(TextTheme base, sh.Theme t) => base.copyWith(
 TextStyle textStyle(sh.Font font, [TextStyle base]) {
   base ??= TextStyle();
   return base.copyWith(
-      fontFamily: font.family,
-      color: color(font.color),
-      fontWeight: fontWeight(font.weight),
-      fontStyle: fontStyle(font.style),
-      decoration: textDecoration(font.decoration),
-      decorationColor: color(font.decoration.color),
-      decorationStyle: textDecorationStyle(font.decoration.style),
-      height: font.height,
-      fontSize: font.size,
-      letterSpacing: font.letterSpacing,
-      wordSpacing: font.wordSpacing);
+      fontFamily: font.family ?? base.fontFamily,
+      color: color(font.color, base.color),
+      fontWeight: fontWeight(font.weight, base.fontWeight),
+      fontStyle: fontStyle(font.style, base.fontStyle),
+      decoration: textDecoration(font.decoration, base.decoration),
+      decorationColor: color(font.decoration?.color, base.decorationColor),
+      decorationStyle:
+          textDecorationStyle(font.decoration?.style, base.decorationStyle),
+      height: font.height ?? base.height,
+      fontSize: font.size ?? base.fontSize,
+      letterSpacing: font.letterSpacing ?? base.letterSpacing,
+      wordSpacing: font.wordSpacing ?? base.wordSpacing);
 }
 
-TextDecoration textDecoration(sh.TextDecoration decoration) =>
-    TextDecoration.combine(
-        decoration.lines.map((line) => textDecorationLine(line)).toList());
+TextDecoration textDecoration(sh.TextDecoration decoration,
+        [TextDecoration fallback]) =>
+    decoration == null
+        ? fallback
+        : TextDecoration.combine(
+            decoration.lines.map((line) => textDecorationLine(line)).toList());
 
 TextDecoration textDecorationLine(sh.TextDecorationLine line) {
   switch (line) {
@@ -115,7 +138,9 @@ TextDecoration textDecorationLine(sh.TextDecorationLine line) {
   }
 }
 
-TextDecorationStyle textDecorationStyle(sh.TextDecorationStyle style) {
+TextDecorationStyle textDecorationStyle(sh.TextDecorationStyle style,
+    [TextDecorationStyle fallback]) {
+  if (style == null) return fallback;
   switch (style) {
     case sh.TextDecorationStyle.dashed:
       return TextDecorationStyle.dashed;
@@ -130,7 +155,8 @@ TextDecorationStyle textDecorationStyle(sh.TextDecorationStyle style) {
   }
 }
 
-FontWeight fontWeight(int weight) {
+FontWeight fontWeight(int weight, [FontWeight fallback]) {
+  if (weight == null) return fallback;
   if (weight < 200) return FontWeight.w100;
   if (weight < 300) return FontWeight.w200;
   if (weight < 400) return FontWeight.w300;
@@ -142,23 +168,30 @@ FontWeight fontWeight(int weight) {
   return FontWeight.w900;
 }
 
-FontStyle fontStyle(sh.FontStyle style) =>
-    style == sh.FontStyle.italic ? FontStyle.italic : FontStyle.normal;
+FontStyle fontStyle(sh.FontStyle style, [FontStyle fallback]) => style == null
+    ? fallback
+    : style == sh.FontStyle.italic ? FontStyle.italic : FontStyle.normal;
 
 Widget buttonWidget(sh.Element button, void Function() onPressed,
-        {String text, Widget child}) =>
-    wrap(
-        RaisedButton(
-          elevation: button.shadow.elevation,
-          onPressed: onPressed,
-          child: child ?? Text(text, style: textStyle(button.font)),
-          color: color(button.color),
-          padding: edgeInsets(button.padding),
-          shape: shapeBorder(button),
-        ),
-        button.margin);
+    {String text, Widget child}) {
+  final base = RaisedButton(onPressed: () {});
+  child ??= button.font == null
+      ? Text(text)
+      : Text(text, style: textStyle(button.font));
+  return wrap(
+      RaisedButton(
+        elevation: button.shadow?.elevation ?? base.elevation,
+        onPressed: onPressed,
+        child: child,
+        color: color(button.color, base.color),
+        padding: edgeInsets(button.padding, base.padding),
+        shape: shapeBorder(button, base.shape),
+      ),
+      button.margin);
+}
 
 /// Wraps [child] in [Padding] if [margin] is non-zero.
-Widget wrap(Widget child, sh.BoxSpacing margin) => margin == sh.BoxSpacing.zero
-    ? Padding(padding: edgeInsets(margin), child: child)
-    : child;
+Widget wrap(Widget child, sh.BoxSpacing margin) =>
+    (margin == null || margin == sh.BoxSpacing.zero)
+        ? child
+        : Padding(padding: edgeInsets(margin), child: child);
